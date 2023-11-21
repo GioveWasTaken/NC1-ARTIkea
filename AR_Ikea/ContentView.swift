52c86ab36c80f874912d75762df13863049aeaa6
Giovanni <giova.bifulco@gmail.com>
2023-11-21 16:22:46 +0100

    Added VoiceOver Lables

--- AR_Ikea/ContentView.swift
+++ AR_Ikea/ContentView.swift
@@ -19,7 +19,8 @@ struct ContentView: View {
     let products: [Product] = [
         Product(name: "Chair", description: "A comfortable chair for your home.", imageName: "chair"),
         Product(name: "Table", description: "A stylish table for your dining room.", imageName: "table"),
-        Product(name: "Sofa", description: "A cozy sofa for your living space.", imageName: "sofa")    ]
+        Product(name: "Sofa", description: "A cozy sofa for your living space.", imageName: "sofa")
+    ]
 
     var body: some View {
         NavigationView {
@@ -27,6 +28,7 @@ struct ContentView: View {
                 NavigationLink(destination: ProductDetailView(product: product)) {
                     ProductRow(product: product)
                 }
+                .accessibility(label: Text("\(product.name): \(product.description)"))
             }
             .navigationTitle("IKEA")
         }
@@ -41,12 +43,17 @@ struct ProductRow: View {
             Image(product.imageName)
                 .resizable()
                 .frame(width: 50, height: 50)
+                .accessibility(label: Text(product.name))
+
             VStack(alignment: .leading) {
                 Text(product.name)
                     .font(.headline)
+                    .accessibility(label: Text("Product Name: \(product.name)"))
+
                 Text(product.description)
                     .font(.subheadline)
                     .foregroundColor(.gray)
+                    .accessibility(label: Text("Description: \(product.description)"))
             }
         }
     }
@@ -62,13 +69,18 @@ struct ProductDetailView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .padding()
+                .accessibility(label: Text(product.name))
+
             Text(product.name)
                 .font(.title)
                 .padding()
+                .accessibility(label: Text("Product Name: \(product.name)"))
+
             Text(product.description)
                 .font(.body)
                 .foregroundColor(.gray)
                 .padding()
+                .accessibility(label: Text("Description: \(product.description)"))
 
             // View in AR Button
             Button("View in AR") {
@@ -78,15 +90,9 @@ struct ProductDetailView: View {
             .sheet(isPresented: $isARViewPresented) {
                 ARViewContainer()
             }
-
+            .accessibility(label: Text("View in AR Button"))
             Spacer()
         }
         .navigationTitle(product.name)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
